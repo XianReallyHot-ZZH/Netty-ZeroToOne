292bed93203bb66f424b5131818397338ea49bff
XianReallyHot-ZZH <741788769@qq.com>
2026-01-18 21:50:22 +0800

    feat:[version_09]完成AbstractChannelHandlerContext内出站类方法链路触发流程实现

--- src/main/java/com/yy/netty/channel/AbstractChannelHandlerContext.java
+++ src/main/java/com/yy/netty/channel/AbstractChannelHandlerContext.java
@@ -3,8 +3,12 @@ package com.yy.netty.channel;
 import com.yy.netty.util.ResourceLeakHint;
 import com.yy.netty.util.concurrent.EventExecutor;
 import com.yy.netty.util.internal.ObjectUtil;
+import com.yy.netty.util.internal.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.SocketAddress;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 import static com.yy.netty.channel.ChannelHandlerMask.*;
 
@@ -23,6 +27,9 @@ public abstract class AbstractChannelHandlerContext implements ChannelHandlerCon
     private static final int REMOVE_COMPLETE = 3;
     // ChannelHandler添加链表后的初始状态
     private static final int INIT = 0;
+
+    private static final AtomicIntegerFieldUpdater<AbstractChannelHandlerContext> HANDLER_STATE_UPDATER =
+            AtomicIntegerFieldUpdater.newUpdater(AbstractChannelHandlerContext.class, "handlerState");
 
     // （双向链表）当前节点在链表上的后一个节点
     volatile AbstractChannelHandlerContext next;
@@ -477,6 +484,481 @@ public abstract class AbstractChannelHandlerContext implements ChannelHandlerCon
     }
 
     // ------------------------------ 出站相关方法 ----------------------------------
+
+    @Override
+    public ChannelHandlerContext read() {
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_READ);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeRead();
+        }
+//        else {
+//            Tasks tasks = next.invokeTasks;
+//            if (tasks == null) {
+//                next.invokeTasks = tasks = new Tasks(next);
+//            }
+//            executor.execute(tasks.invokeReadTask);
+//        }
+
+        return this;
+    }
+
+    private void invokeRead() {
+        if (invokeHandler()) {
+            try {
+                ((ChannelOutboundHandler) handler()).read(this);
+            } catch (Throwable t) {
+                notifyHandlerException(t);
+            }
+        } else {
+            read();
+        }
+    }
+
+    @Override
+    public ChannelHandlerContext flush() {
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_FLUSH);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeFlush();
+        }
+//        else {
+//            Tasks tasks = next.invokeTasks;
+//            if (tasks == null) {
+//                next.invokeTasks = tasks = new Tasks(next);
+//            }
+//            safeExecute(executor, tasks.invokeFlushTask, channel().voidPromise(), null);
+//        }
+        return this;
+    }
+
+    private void invokeFlush() {
+        if (invokeHandler()) {
+            //发送缓冲区的数据
+            invokeFlush0();
+        } else {
+            flush();
+        }
+    }
+
+    private void invokeFlush0() {
+        try {
+            ((ChannelOutboundHandler) handler()).flush(this);
+        } catch (Throwable t) {
+            notifyHandlerException(t);
+        }
+    }
+
+    /**
+     * 每当我调用一个方法时，比如说就是服务端channel的绑定端口号的bind方法，调用链路会先从AbstractChannel类中开始，
+     * 但是，channel拥有ChannelPipeline链表，链表中有一系列的处理器，所以调用链就会跑到ChannelPipeline中，然后从ChannelPipeline
+     * 又跑到每一个ChannelHandler中，经过这些ChannelHandler的处理，调用链又会跑到channel的内部类Unsafe中，再经过一系列的调用，
+     * 最后来到NioServerSocketChannel中，执行真正的doBind方法。
+     * @param localAddress
+     * @return
+     */
+    @Override
+    public ChannelFuture bind(SocketAddress localAddress) {
+        return bind(localAddress, newPromise());
+    }
+
+    @Override
+    public ChannelFuture bind(SocketAddress localAddress, ChannelPromise promise) {
+        if (localAddress == null) {
+            throw new NullPointerException("localAddress");
+        }
+        //找到对bind事件感兴趣的handler
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_BIND);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            //这个时候肯定是单线程执行器接管了channel，所以会走这个分支
+            next.invokeBind(localAddress, promise);
+        } else {
+            safeExecute(executor, new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeBind(localAddress, promise);
+                }
+            }, promise, null);
+        }
+        return promise;
+    }
+
+    private void invokeBind(SocketAddress localAddress, ChannelPromise promise) {
+        if (invokeHandler()) {
+            try {
+                //每次都要调用handler()方法来获得handler，但是接口中的handler方法是在哪里实现的呢？
+                //在DefaultChannelHandlerContext类中，这也提醒着我们，我们创建的context节点是DefaultChannelHandlerContext节点。
+                ((ChannelOutboundHandler) handler()).bind(this, localAddress, promise);
+            } catch (Throwable t) {
+                notifyOutboundHandlerException(t, promise);
+            }
+        } else {
+            bind(localAddress, promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture connect(SocketAddress remoteAddress) {
+        return connect(remoteAddress, newPromise());
+    }
+
+    @Override
+    public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress) {
+        return connect(remoteAddress, localAddress, newPromise());
+    }
+
+    @Override
+    public ChannelFuture connect(SocketAddress remoteAddress, ChannelPromise promise) {
+        return connect(remoteAddress, null, promise);
+    }
+
+    @Override
+    public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+        if (remoteAddress == null) {
+            throw new NullPointerException("remoteAddress");
+        }
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_CONNECT);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeConnect(remoteAddress, localAddress, promise);
+        } else {
+            safeExecute(executor, new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeConnect(remoteAddress, localAddress, promise);
+                }
+            }, promise, null);
+        }
+        return promise;
+    }
+
+    private void invokeConnect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+        if (invokeHandler()) {
+            try {
+                ((ChannelOutboundHandler) handler()).connect(this, remoteAddress, localAddress, promise);
+            } catch (Throwable t) {
+                notifyOutboundHandlerException(t, promise);
+            }
+        } else {
+            connect(remoteAddress, localAddress, promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture disconnect() {
+        return disconnect(newPromise());
+    }
+
+    @Override
+    public ChannelFuture disconnect(ChannelPromise promise) {
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_DISCONNECT);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeDisconnect(promise);
+        } else {
+            safeExecute(executor, new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeDisconnect(promise);
+                }
+            }, promise, null);
+        }
+        return promise;
+    }
+
+    private void invokeDisconnect(ChannelPromise promise) {
+        if (invokeHandler()) {
+            try {
+                ((ChannelOutboundHandler) handler()).disconnect(this, promise);
+            } catch (Throwable t) {
+                notifyOutboundHandlerException(t, promise);
+            }
+        } else {
+            disconnect(promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture close() {
+        return close(newPromise());
+    }
+
+    // 关闭连接的方法，这个方法会放在最后优雅停机和释放资源的时候进行串联
+    @Override
+    public ChannelFuture close(ChannelPromise promise) {
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_CLOSE);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeClose(promise);
+        } else {
+            safeExecute(executor, new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeClose(promise);
+                }
+            }, promise, null);
+        }
+
+        return promise;
+    }
+
+    private void invokeClose(ChannelPromise promise) {
+        if (invokeHandler()) {
+            try {
+                ((ChannelOutboundHandler) handler()).close(this, promise);
+            } catch (Throwable t) {
+                notifyOutboundHandlerException(t, promise);
+            }
+        } else {
+            close(promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture deregister() {
+        return deregister(newPromise());
+    }
+
+    @Override
+    public ChannelFuture deregister(ChannelPromise promise) {
+        if (isNotValidPromise(promise, false)) {
+            return promise;
+        }
+        final AbstractChannelHandlerContext next = findContextOutbound(MASK_DEREGISTER);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            next.invokeDeregister(promise);
+        } else {
+            safeExecute(executor, new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeDeregister(promise);
+                }
+            }, promise, null);
+        }
+
+        return promise;
+    }
+
+    private void invokeDeregister(ChannelPromise promise) {
+        if (invokeHandler()) {
+            try {
+                ((ChannelOutboundHandler) handler()).deregister(this, promise);
+            } catch (Throwable t) {
+                notifyOutboundHandlerException(t, promise);
+            }
+        } else {
+            deregister(promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture write(Object msg) {
+        return write(msg, newPromise());
+    }
+
+    @Override
+    public ChannelFuture write(Object msg, ChannelPromise promise) {
+        write(msg, false, promise);
+        return promise;
+    }
+
+    private void invokeWrite(Object msg, ChannelPromise promise) {
+        if (invokeHandler()) {
+            invokeWrite0(msg, promise);
+        } else {
+            write(msg, promise);
+        }
+    }
+
+    private void invokeWrite0(Object msg, ChannelPromise promise) {
+        try {
+            ((ChannelOutboundHandler) handler()).write(this, msg, promise);
+        } catch (Throwable t) {
+            notifyOutboundHandlerException(t, promise);
+        }
+    }
+
+    @Override
+    public ChannelFuture writeAndFlush(Object msg) {
+        return writeAndFlush(msg, newPromise());
+    }
+
+    @Override
+    public ChannelFuture writeAndFlush(Object msg, ChannelPromise promise) {
+        write(msg, true, promise);
+        return promise;
+    }
+
+    private void invokeWriteAndFlush(Object msg, ChannelPromise promise) {
+        if (invokeHandler()) {
+            invokeWrite0(msg, promise);
+            invokeFlush0();
+        } else {
+            writeAndFlush(msg, promise);
+        }
+    }
+
+    @Override
+    public ChannelPromise newPromise() {
+        return new DefaultChannelPromise(channel(), executor());
+    }
+
+    // 该方法做了一点小改动，我没有引入SucceededChannelFuture类，不是核心方法，看看就行
+    @Override
+    public ChannelFuture newSucceededFuture() {
+        ChannelFuture succeededFuture = this.succeededFuture;
+        if (succeededFuture == null) {
+            this.succeededFuture = succeededFuture = new DefaultChannelPromise(channel(), executor());
+        }
+        return succeededFuture;
+    }
+
+    @Override
+    public ChannelFuture newFailedFuture(Throwable cause) {
+        //return new FailedChannelFuture(channel(), executor(), cause);
+        return null;
+    }
+
+    private AbstractChannelHandlerContext findContextOutbound(int mask) {
+        AbstractChannelHandlerContext ctx = this;
+        do {
+            //为什么获取前一个？因为是出站处理器，数据从后往前传输
+            ctx = ctx.prev;
+            //做&运算，判断事件合集中是否包含该事件
+        } while ((ctx.executionMask & mask) == 0);
+        return ctx;
+    }
+
+    private static void notifyOutboundHandlerException(Throwable cause, ChannelPromise promise) {
+        //PromiseNotificationUtil.tryFailure(promise, cause, promise instanceof VoidChannelPromise ? null : logger);
+    }
+
+    private boolean isNotValidPromise(ChannelPromise promise, boolean allowVoidPromise) {
+        if (promise == null) {
+            throw new NullPointerException("promise");
+        }
+        if (promise.isDone()) {
+            if (promise.isCancelled()) {
+                return true;
+            }
+            throw new IllegalArgumentException("promise already done: " + promise);
+        }
+        if (promise.channel() != channel()) {
+            throw new IllegalArgumentException(String.format("promise.channel does not match: %s (expected: %s)", promise.channel(), channel()));
+        }
+        if (promise.getClass() == DefaultChannelPromise.class) {
+            return false;
+        }
+        if (promise instanceof AbstractChannel.CloseFuture) {
+            throw new IllegalArgumentException(StringUtil.simpleClassName(AbstractChannel.CloseFuture.class) + " not allowed in a pipeline");
+        }
+        return false;
+    }
+
+    private void write(Object msg, boolean flush, ChannelPromise promise) {
+        ObjectUtil.checkNotNull(msg, "msg");
+        final AbstractChannelHandlerContext next = findContextOutbound(flush ?
+                (MASK_WRITE | MASK_FLUSH) : MASK_WRITE);
+        final Object m = msg;
+        //该方法用来检查内存是否泄漏，因为还未引入，所以暂时注释掉
+        //final Object m = pipeline.touch(msg, next);
+        EventExecutor executor = next.executor();
+        if (executor.inEventLoop(Thread.currentThread())) {
+            if (flush) {
+                //flush为true，所以进入这个分支
+                next.invokeWriteAndFlush(m, promise);
+            } else {
+                next.invokeWrite(m, promise);
+            }
+        } else {
+            //下面被注释掉的分支是源码，这个else分支是我自己写的，等后面再真正实现
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    next.invokeWriteAndFlush(m, promise);
+                }
+            });
+        }
+//        else {
+//            final AbstractWriteTask task;
+//            if (flush) {
+//                task = WriteAndFlushTask.newInstance(next, m, promise);
+//            }  else {
+//                task = WriteTask.newInstance(next, m, promise);
+//            }
+//            if (!safeExecute(executor, task, promise, m)) {
+//                task.cancel();
+//            }
+//        }
+    }
+
+
+
+    // -------------------------------------- 内部通用的、基础的方法 --------------------------------------
+    private static boolean safeExecute(EventExecutor executor, Runnable runnable, ChannelPromise promise, Object msg) {
+        try {
+            executor.execute(runnable);
+            return true;
+        } catch (Throwable cause) {
+            try {
+                promise.setFailure(cause);
+            } finally {
+                if (msg != null) {
+                    //当该引用计数减至为0时，该ByteBuf即可回收，我们还未讲到这里，所以我先注释掉这个方法
+                    //ReferenceCountUtil.release(msg);
+                }
+            }
+            return false;
+        }
+    }
+
+    // 把链表中的ChannelHandler的状态设置为删除完成
+    final void setRemoved() {
+        handlerState = REMOVE_COMPLETE;
+    }
+
+    // 把链表中的ChannelHandler的状态设置为添加完成
+    final boolean setAddComplete() {
+        for (;;) {
+            int oldState = handlerState;
+            if (oldState == REMOVE_COMPLETE) {
+                return false;
+            }
+            if (HANDLER_STATE_UPDATER.compareAndSet(this, oldState, ADD_COMPLETE)) {
+                return true;
+            }
+        }
+    }
+
+    // 把链表中的ChannelHandler的状态设置为等待添加
+    final void setAddPending() {
+        boolean updated = HANDLER_STATE_UPDATER.compareAndSet(this, INIT, ADD_PENDING);
+        assert updated;
+    }
+
+    // 在该方法中，ChannelHandler的添加状态将变为添加完成，然后ChannelHandler调用它的handlerAdded方法
+    final void callHandlerAdded() throws Exception {
+        //在这里改变channelhandler的状态
+        if (setAddComplete()) {
+            handler().handlerAdded(this);
+        }
+    }
+
+    // 回调链表中节点的handlerRemoved方法，该方法在ChannelPipeline中有节点被删除时被调用。
+    final void callHandlerRemoved() throws Exception {
+        try {
+            if (handlerState == ADD_COMPLETE) {
+                handler().handlerRemoved(this);
+            }
+        } finally {
+            setRemoved();
+        }
+    }
+
+
+
 
 
 }

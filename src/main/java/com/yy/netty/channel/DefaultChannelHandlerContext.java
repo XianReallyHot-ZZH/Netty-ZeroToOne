035399b92c64433e73079f60ece04ec476dd5aa0
XianReallyHot-ZZH <741788769@qq.com>
2026-01-18 21:58:22 +0800

    feat:[version_09]提供DefaultChannelHandlerContext默认实现

---
+++ src/main/java/com/yy/netty/channel/DefaultChannelHandlerContext.java
@@ -0,0 +1,30 @@
+package com.yy.netty.channel;
+
+import com.yy.netty.util.concurrent.EventExecutor;
+
+/**
+ * 默认的ChannelHandlerContext, 以组合的方式，内部持有一个ChannelHandler
+ */
+public class DefaultChannelHandlerContext extends AbstractChannelHandlerContext {
+
+    // 内部持有一个ChannelHandler，组合的方式
+    private final ChannelHandler handler;
+
+    /**
+     * 构造方法
+     *
+     * @param pipeline
+     * @param executor
+     * @param name
+     * @param handler   外部传入的ChannelHandler
+     */
+    DefaultChannelHandlerContext(DefaultChannelPipeline pipeline, EventExecutor executor, String name, ChannelHandler handler) {
+        super(pipeline, executor, name, handler.getClass());
+        this.handler = handler;
+    }
+
+    @Override
+    public ChannelHandler handler() {
+        return handler;
+    }
+}
